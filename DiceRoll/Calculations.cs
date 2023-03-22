090b0116c12b4db8514a5241476e488f4d894444
Rhystic1 <Rhystic1@users.noreply.github.com>
2023-03-22 19:46:57 +0000

    Add project files.

---
+++ DiceRoll/Calculations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoll
+{
+    internal class Calculations
+    {
+        Random rnd = new Random();
+        int min = 1;
+        int max;
+        
+        public int SelectDie()
+        {
+            Console.WriteLine("Please type in the number of the die that you need.");
+            try
+            {
+                max = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You must select a valid number.");
+                SelectDie();
+            }
+
+            return max;
+        }
+        public int GenerateResult(int min, int max)
+        {
+            int randomResult = rnd.Next(min, max);
+            Console.WriteLine("You rolled a... " + randomResult);
+            Console.WriteLine("");
+            Console.WriteLine("Do you want to roll another die? Press any key except N to proceed.");
+            Console.WriteLine("");
+            return randomResult;
+        }
+    }
+}
---
+++ DiceRoll/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DiceRoll;
+using System.Runtime.CompilerServices;
+
+Calculations calculations = new Calculations();
+bool programRunning = true;
+int min = 1;
+while (programRunning)
+{
+    int max = calculations.SelectDie();
+    calculations.GenerateResult(min, max);
+
+    char userResponse = Console.ReadKey(true).KeyChar;
+    if (userResponse == 'n')
+    {
+        programRunning = false;
+    }
+}
+
+
